6bba8500e45e78d3f0cf8bacb27efa0694f21775
wills blake <richwblake@gmail.com>
2022-04-26 09:59:37 -0700

    Blank state

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
-// This file is just a reglular javascript file inside a folder. 
+// This file is just a reglular javascript file inside a folder.
 // Our goal is to change this project from arbitrary files and folders to an npm (nodejs) project!
+
